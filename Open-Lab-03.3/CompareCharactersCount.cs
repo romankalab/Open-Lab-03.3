829810238db7a7c11df21b5b897feb171010a27c
romankalab <roan.kalab@gmail.com>
2019-10-24 08:51:52 +0200

    Initial

---
+++ Open-Lab-03.3/CompareCharactersCount.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CompareCharactersCount
+{
+    class Compare
+    {
+        public bool CompareLenght(string word1, string word2)
+        {
+            return word1.Length == word2.Length;
+        }
+    }
+}
---
+++ Open-Lab-03.3/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using CompareCharactersCount;
+
+namespace Open_Lab_03._3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Write the first word");
+            string word1 = Console.ReadLine();
+
+            Console.WriteLine("Write the second word");
+            string word2 = Console.ReadLine();
+
+            Compare compare = new Compare();
+            bool result = compare.CompareLenght(word1, word2);
+            Console.WriteLine(result);
+        }
+    }
+}
